a80d07845f8fb806079febd54292d3caccb96ae5
rihab397 <porwalrishab49@gmail.com>
2023-01-11 12:03:55 +0530

    ss

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var path=require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+require('dotenv').config();
 app.set('view engine', 'hbs');
 // app.set('views', path.join(__dirname, 'views/'));
+var PORT =   process.env.PORT || 1337 ||5000 ||2000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -26,7 +28,7 @@ app.use("/investors",unpaidInvestorRouter)
 app.use("/user",userRouter)
 app.use("/Career",Career)
 app.listen(4000, () => {
-    console.log('server is running on ' + 4000);
+    console.log('server is running on ' + PORT);
 })
 
 
